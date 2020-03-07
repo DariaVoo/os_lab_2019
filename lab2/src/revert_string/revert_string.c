984b937cf9f3e2ca8b862875d628d8b6427db3de
Daria Vorontsova <dasha2003.voo@yandex.ru>
2020-03-07 11:45:29 +0300

    lab2 revert_str

--- lab2/src/revert_string/revert_string.c
+++ lab2/src/revert_string/revert_string.c
@@ -2,6 +2,22 @@
 
 void RevertString(char *str)
 {
-	// your code here
+	size_t	len;
+	size_t	first;
+	size_t	middle;
+	char	buf;
+
+	first = 0;
+	len = ft_strlen(str) - 1;
+	middle = len / 2 + len % 2;
+	while (first != middle)
+	{
+		buf = str[first];
+		str[first] = str[len];
+		str[len] = buf;
+		first++;
+		len--;
+	}
+	return ;
 }
 
--- lab2/src/revert_string/revert_string.h
+++ lab2/src/revert_string/revert_string.h
@@ -1,4 +1,11 @@
+#ifndef FILLER_H
+# define FILLER_H
+
+#include <stdio.h>
+#include <wchar.h>
 
 /* function to revert string */
-void RevertString(char *str);
+void	RevertString(char *str);
+size_t	ft_strlen(const char *str);
 
+#endif
