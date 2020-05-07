1b91a17d1b670b69d97facc55929fb3226babd31
Daria Vorontsova <dasha2003.voo@yandex.ru>
2020-05-07 21:13:37 +0300

    fix parse

--- lab5/src/factorial.c
+++ lab5/src/factorial.c
@@ -1,14 +1,15 @@
+#include <zconf.h>
 #include "factorial.h"
 
 int parse(int argc, char **argv, int *k, int *pnum, int *mod)
 {
-	char *usage = "Usage: %s -k \"num\" --pnum \"num\" --mod \"num\"\n";
+	char *usage = "Usage: %s -k \"num\" --pnum=\"num\" --mod=\"num\"\n";
 
 	while (1)
 	{
 		/*optid - индекс следующего элемента, который будет отсканирован*/
 		static struct option options[] = {
-				{"k", required_argument, 0, 'k'},
+//				{"k", required_argument, 0, 'k'},
 				{"pnum", required_argument, 0, 0},
 				{"mod", required_argument, 0, 0},
 				//для однозначного определения конца массива
@@ -16,20 +17,17 @@ int parse(int argc, char **argv, int *k, int *pnum, int *mod)
 		};
 		/*Указатель на переменную, в которую будет помещён
 		  индекс текущего параметра из массива options*/
-		int option_index = 0;
+		int longindex = 0;
 		/*Парсим параметры*/
-		int c = getopt_long(argc, argv, "f", options, &option_index);
+		int c = getopt_long(argc, argv, "k:", options, &longindex);
 		if (c == -1)
 			break;
 		switch (c)
 		{
 			case 0:
-				switch (option_index)
+				switch (longindex)
 				{
 					case 0:
-						*k = atoi(optarg);
-						break;
-					case 1:
 						*pnum = atoi(optarg);
 						if (*pnum <= 0)
 						{
@@ -37,7 +35,7 @@ int parse(int argc, char **argv, int *k, int *pnum, int *mod)
 							return 1;
 						}
 						break;
-					case 2:
+					case 1:
 						*mod = atoi(optarg);
 						if (*mod <= 0)
 						{
@@ -48,11 +46,15 @@ int parse(int argc, char **argv, int *k, int *pnum, int *mod)
 				}
 				break;
 
+			case 'k':
+				*k = atoi(optarg);
+				break;
+
 			case '?':
 				break;
 
 			default:
-				printf("getopt returned character code 0%o?\n", c);
+				printf("getopt returned character code 0%o(%c)?\n", c, c);
 		}
 	}
 	if (optind < argc) {
@@ -67,13 +69,13 @@ int parse(int argc, char **argv, int *k, int *pnum, int *mod)
 }
 
 int main(int argc, char **argv) {
-	int threads_num = 0;
-	int array_size = 0;
-	int seed = 0;
+	int k = 0;
+	int pnum = 0;
+	int mod = 0;
 
-	if (parse(argc, argv, &threads_num, &array_size, &seed))
+	if (parse(argc, argv, &k, &pnum, &mod))
 		return (1);
 	else
-		printf("%d %d %d\n", threads_num, seed, array_size);
+		printf("%d %d %d\n", k, pnum, mod);
 	return (0);
 }
